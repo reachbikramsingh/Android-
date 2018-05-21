816933772db86856b81fcf9b47a711a8801a501c
reachbikramsingh <reachbikramsingh@gmail.com>
2018-05-21 18:55:10 +0530

    Initial commit

---
+++ app/src/main/java/deccansoft/com/qrbarcodescanner/QRActivity.java
@@ -0,0 +1,14 @@
+package deccansoft.com.qrbarcodescanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class QRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+    }
+}
